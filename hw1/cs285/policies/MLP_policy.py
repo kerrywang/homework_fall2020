ce9a6b39d1430937363c7af5d5a39bc3711da1d7
Kaiyue Wang <kaiyuewang123@gmail.com>
2021-08-26 23:13:32 -0700

    at least it compiles

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -80,8 +80,9 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         else:
             observation = obs[None]
 
-        # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        observation = ptu.from_numpy(observation.astype(np.float32))
+        action = self(observation)  # this is essentially invoking the inferrence of Neural Network.
+        return ptu.to_numpy(action)
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -109,8 +110,18 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+        pred = self(ptu.from_numpy(observations))
+        loss = self.loss(pred, ptu.from_numpy(actions))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
         }
+
+    def forward(self, observation: torch.FloatTensor) -> Any:
+        if self.discrete:
+            return distributions.categorical.Categorical(self.logits_na(observation)).sample()
+        return distributions.Normal(self.mean_net(observation), torch.exp(self.logstd)).rsample()
